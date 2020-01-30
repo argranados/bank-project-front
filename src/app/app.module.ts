d3abab88981e9e43a8649eaebfde1b97968e25bc
Alfonso Granados <argranados@yahoo.com>
2020-01-29 21:50:57 -0600

    add nav and client, accounts and products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ClientsComponent } from './clients/clients.component';
+import { AccountsComponent } from './accounts/accounts.component';
+import { ProductsComponent } from './products/products.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientsComponent,
+    AccountsComponent,
+    ProductsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
